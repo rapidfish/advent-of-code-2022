cd69daabc415ceadf67c289a70e00c5646ed8d7d
Oskar Bergström <rapidfish@me.com>
2022-12-01 22:31:16 +0100

    Update IResolveTasks.java
    tog bort kommentarer

--- src/main/java/se/osbe/aoc/IResolveTasks.java
+++ src/main/java/se/osbe/aoc/IResolveTasks.java
@@ -3,13 +3,10 @@ package se.osbe.aoc;
 import static se.osbe.aoc.helper.SantasLittleHelper.printResult;
 
 public interface IResolveTasks {
-    //public void resolveTask1() throws Exception;
 
     default void resolveTask1() throws Exception {
         printResult("--", "1", "(OBS! - Ej impl. ännu...)");
     }
-
-    //void resolveTask2() throws Exception;
 
     default void resolveTask2() throws Exception {
         printResult("--", "2", "(OBS! - Ej impl. ännu...)");
